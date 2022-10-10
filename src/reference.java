4ea496d52ea3b601e619e7938ae8193bc8cd7325
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-10-10 14:45:46 +0900

    주어진 int형 배열에서, 엄격하게 오름차순인 부분 배열의 최대길이

--- src/reference.java
+++ src/reference.java
@@ -11,15 +11,15 @@ public class reference {
 
     public static int LIS(int[] arr) {
         int N = arr.length;
-        int[] lis = new int[N];
-        Arrays.fill(lis, 1);
+        int[] lis = new int[N]; // 배열의 특정 인자까지 도달할 수 있는 경우를 카운트하는 배열
+        Arrays.fill(lis, 1); // 자기 자신만 카운트하는건 무조건 있으니 전부 1가지 경우는 있는걸로 시작
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
                 if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
-                    lis[i] = lis[j] + 1;
+                    lis[i] = lis[j] + 1; // 만약 무탈히 도달하면 카운트 +1
                 }
             }
         }
-        return Arrays.stream(lis).max().getAsInt();
+        return Arrays.stream(lis).max().getAsInt();  // lis에서 카운트 한 애들 중 가장 큰 애 리턴
     }
 }
